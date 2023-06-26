9bdafaba318705d2c98a4241af54f49bc0d315e2
Arkantik <95821033+Arkantik@users.noreply.github.com>
2023-06-26 04:14:56 +0200

    Reduction of pages to one

---
+++ frontend/src/pages/Portfolio.jsx
@@ -0,0 +1,15 @@
+import Home from "../components/Home";
+import Project from "../components/Project";
+import Recommendation from "../components/Recommendation";
+// import Timeline from "../components/Timeline";
+
+export default function Portfolio() {
+  return (
+    <div>
+      <Home />
+      {/* <Timeline /> */}
+      <Project />
+      <Recommendation />
+    </div>
+  );
+}
