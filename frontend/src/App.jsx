9b8aafa6afb8c5754a3a851cac8e60a47c0c4ae2
Arkantik <95821033+Arkantik@users.noreply.github.com>
2023-06-26 04:10:49 +0200

    Added routes and components in app.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,14 +1,53 @@
-import Home from "./pages/Home";
+// Packages
+import { useState, useEffect } from "react";
+import { Routes, Route } from "react-router-dom";
+import PulseLoader from "react-spinners/PulseLoader";
+
+// Components
+import Portfolio from "./pages/Portfolio";
+import Navbar from "./components/Navbar";
 
 import "./App.css";
 
-function App() {
+export default function App() {
+  const [loading, setLoading] = useState(true);
+
+  const override = {
+    display: "flex",
+    justifyContent: "center",
+    alignItems: "center",
+    width: "100%",
+    height: "100vh",
+    gap: "16px",
+  };
+
+  useEffect(() => {
+    setLoading(true);
+    setTimeout(() => {
+      setLoading(false);
+    }, 3000);
+  }, []);
+
   return (
-    <div className="App">
-      <Home />
-      <p>coucou</p>
+    <div className="bg-dark">
+      {loading ? (
+        <PulseLoader
+          color="#26acd9"
+          cssOverride={override}
+          loading={loading}
+          size={15}
+          speedMultiplier={0.5}
+        />
+      ) : (
+        <>
+          <Navbar />
+          <main>
+            <Routes>
+              <Route path="/" element={<Portfolio />} />
+            </Routes>
+          </main>
+        </>
+      )}
     </div>
   );
 }
-
-export default App;
