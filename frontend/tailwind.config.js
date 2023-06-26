eafa4deba6a94ad2ed62885c9255cb31225ebcae
Arkantik <95821033+Arkantik@users.noreply.github.com>
2023-06-26 04:01:03 +0200

    Tailwind config file

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -1,8 +1,64 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./src/**/*.{html,js, jsx}"],
+  content: ["./src/**/*.{html,js,jsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: "#26acd9",
+        dark: "#171717",
+        light: "#ffffff",
+      },
+
+      fontFamily: {
+        main: ['"Oswald"', "sans-serif"],
+        alt: ['"Roboto"', "sans-serif"],
+      },
+
+      fontSize: {
+        "2xl": [
+          "4.5rem",
+          {
+            lineHeight: "1.2",
+            fontWeight: "900",
+          },
+        ],
+        xl: [
+          "2rem",
+          {
+            lineHeight: "1.2",
+            fontWeight: "900",
+          },
+        ],
+        lg: [
+          "1.5rem",
+          {
+            lineHeight: "1.75",
+            fontWeight: "400",
+          },
+        ],
+        base: [
+          "1rem",
+          {
+            lineHeight: "1.75",
+            fontWeight: "400",
+          },
+        ],
+        sm: [
+          "0.875rem",
+          {
+            lineHeight: "1.5",
+            fontWeight: "300",
+          },
+        ],
+        xs: [
+          "0.75rem",
+          {
+            lineHeight: "1.5",
+            fontWeight: "300",
+          },
+        ],
+      },
+    },
+    plugins: [],
   },
-  plugins: [],
 };
