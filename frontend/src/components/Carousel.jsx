33de4d71f530e1dde866431d2eca39fdbd5bfa5a
Arkantik <95821033+Arkantik@users.noreply.github.com>
2023-06-26 12:19:09 +0200

    Creation of carousel for project via swiper

---
+++ frontend/src/components/Carousel.jsx
@@ -0,0 +1,94 @@
+// Package
+import { Swiper, SwiperSlide } from "swiper/react";
+import SwiperCore, {
+  EffectCoverflow,
+  Pagination,
+  Navigation,
+  Autoplay,
+} from "swiper";
+
+// Style
+import "swiper/swiper-bundle.min.css";
+import style from "../../css/project.module.css";
+
+// Data
+import projectData from "../services/project.json";
+
+SwiperCore.use([EffectCoverflow, Pagination, Navigation, Autoplay]);
+
+export default function Carousel() {
+  const slideImages = projectData
+    .slice(0, 7)
+    .map(({ id, src, name, techno, description }) => ({
+      id,
+      src,
+      name,
+      techno,
+      description,
+    }));
+
+  return (
+    <>
+      <Swiper
+        effect="coverflow"
+        grabCursor
+        centeredSlides
+        initialSlide={1}
+        slidesPerView="auto"
+        coverflowEffect={{
+          rotate: 0,
+          stretch: 0,
+          depth: 125,
+          modifier: 2.5,
+        }}
+        pagination={{ el: ".pagination", clickable: true }}
+        navigation={{
+          nextEl: ".button-next",
+          prevEl: ".button-prev",
+          clickable: true,
+        }}
+        className={`${style.swiper_container}`}
+        autoplay={{
+          delay: 5000,
+          disableOnInteraction: false,
+        }}
+      >
+        {slideImages.map(({ id, src, name, techno, description }) => (
+          <SwiperSlide key={id}>
+            <img src={src} alt={name} />
+            {Object.values(techno).map((tech) => (
+              <img
+                key={tech}
+                src={tech}
+                alt={tech}
+                className="inline-flex p-2"
+              />
+            ))}
+            <figcaption className="absolute bottom-0 w-full rounded-b-2xl p-2 text-center text-base text-dark">
+              {description}
+            </figcaption>
+          </SwiperSlide>
+        ))}
+      </Swiper>
+      <div className="swiper-controller flex items-center justify-center">
+        <div className="flex items-center gap-4">
+          <button type="button" className="button-prev">
+            <img
+              src="../assets/icons/portfolio/arrow-left-circle.svg"
+              alt="Previous Slide"
+              className="h-10 w-10"
+            />
+          </button>
+          <div className="pagination flex items-center justify-around gap-1" />
+          <button type="button" className="button-next">
+            <img
+              src="../assets/icons/portfolio/arrow-right-circle.svg"
+              alt="Next Slide"
+              className="h-10 w-10"
+            />
+          </button>
+        </div>
+      </div>
+    </>
+  );
+}
--- frontend/src/components/Project.jsx
+++ frontend/src/components/Project.jsx
@@ -1,194 +1,184 @@
+import Carousel from "./Carousel";
+
 export default function Project() {
   return (
-    <section className="section portfolio-bg slide4" id="project">
-      <div className="container">
-        <div className="row heading-row pb-5">
-          <div className="col-12 col-md-7">
-            <h3 className="main-font text-uppercase">
-              <span className="text-blue d-block"> Portfolio </span>
-              <span> of my projects </span>
-            </h3>
-          </div>
-        </div>
-        <div className="row">
-          <div className="col-sm-12">
-            <div className="team-classic owl-team owl-carousel text-center">
-              <div className="item">
-                <div className="team-data-img">
-                  <a
-                    href="https://arkantik.github.io/WildBook/"
-                    target="_blank"
-                    rel="noreferrer"
-                  >
-                    <div className="single-work">
-                      <img
-                        src="./public/assets/img/project/wildbook.png"
-                        alt="Wildbook project"
-                        className="img-responsive"
-                        data-no-retina
-                      />
-                      <div className="overlay-text center-block">
-                        <div className="cases-image-inner">
-                          <span className="cases-line top" />
-                          <span className="cases-line top-right" />
-                          <span className="cases-line bottom" />
-                          <span className="cases-line bottom-left" />
-                          <h6 className="text-uppercase alt-font text-light">
-                            WildBook
-                          </h6>
-                        </div>
-                      </div>
-                    </div>
-                  </a>
-                  <div className="portfolio-content">
-                    <h5 className="text-blue main-font text-uppercase mb-2">
-                      WildBook - My First Website
-                    </h5>
-                    <p>
-                      Responsive and mobile-first single-page site using HTML,
-                      CSS, and vanilla JS. <br />
-                      This project was made in 2 weeks with only 2 weeks of
-                      experience in web dev
-                    </p>
-                  </div>
-                </div>
-              </div>
+    <section
+      className="flex flex-col justify-center gap-8 font-alt"
+      id="project"
+    >
+      <div>
+        <h2 className="block font-main text-xl uppercase text-light">
+          <span className="block uppercase text-primary"> Portfolio </span>
+          <span> of my projects </span>
+        </h2>
+      </div>
 
-              <div className="item">
-                <div className="team-data-img">
-                  <a
-                    href="https://arkantik.github.io/hogwarts-magic-cards/"
-                    target="_blank"
-                    rel="noreferrer"
-                  >
-                    <div className="single-work">
-                      <img
-                        src="./public/assets/img/project/hogwarts.png"
-                        alt="Hogwarts magic cards project"
-                        className="img-responsive"
-                        data-no-retina
-                      />
-                      <div className="overlay-text center-block">
-                        <div className="cases-image-inner">
-                          <span className="cases-line top" />
-                          <span className="cases-line top-right" />
-                          <span className="cases-line bottom" />
-                          <span className="cases-line bottom-left" />
-                          <h6 className="text-uppercase alt-font text-light">
-                            Hogwarts Magic Cards
-                          </h6>
-                        </div>
+      <Carousel />
+      {/* <div className="row">
+        <div className="col-sm-12">
+          <div className="team-classic owl-team owl-carousel text-center">
+            <div className="item">
+              <div className="team-data-img">
+                <a
+                  href="https://arkantik.github.io/WildBook/"
+                  target="_blank"
+                  rel="noreferrer"
+                >
+                  <div className="single-work">
+                    <img
+                      src="../assets/img/project/wildbook.png"
+                      alt="Wildbook project"
+                      className="img-responsive"
+                      data-no-retina
+                    />
+                    <div className="overlay-text center-block">
+                      <div className="cases-image-inner">
+                        <span className="cases-line top" />
+                        <span className="cases-line top-right" />
+                        <span className="cases-line bottom" />
+                        <span className="cases-line bottom-left" />
+                        <h6 className="text-uppercase alt-font text-light">
+                          WildBook
+                        </h6>
                       </div>
                     </div>
-                  </a>
-                  <div className="portfolio-content">
-                    <h5 className="text-blue main-font text-uppercase mb-2">
-                      Hogwarts Magic Cards
-                    </h5>
-                    <p>
-                      Flash-game type website using ReactJS, a CSS
-                      framework(Tailwind), and integrating an API.
-                      <br />
-                      This website is a strategy turn by turn card game set in
-                      the world of Harry Potter.
-                    </p>
                   </div>
-                </div>
-              </div>
-
-              <div className="item">
-                <div className="team-data-img">
-                  <a
-                    href="https://arkantik.github.io/holifun/"
-                    target="_blank"
-                    rel="noreferrer"
-                  >
-                    <div className="single-work">
-                      <img
-                        src="./public/assets/img/project/holifun.png"
-                        alt="Holifun project"
-                        className="img-responsive"
-                        data-no-retina
-                      />
-                      <div className="overlay-text center-block">
-                        <div className="cases-image-inner">
-                          <span className="cases-line top" />
-                          <span className="cases-line top-right" />
-                          <span className="cases-line bottom" />
-                          <span className="cases-line bottom-left" />
-                          <h6 className="text-uppercase alt-font text-light">
-                            HoliFun
-                          </h6>
-                        </div>
-                      </div>
-                    </div>
-                  </a>
-                  <div className="portfolio-content">
-                    <h5 className="text-blue main-font text-uppercase mb-2">
-                      HoliFun
-                    </h5>
-                    <p>
-                      No idea where to go for your next vacation? HoliFun is
-                      made for you!
-                      <br />
-                      This project has been created during a 36h Hackathon, with
-                      the objective to build something about vacation and using
-                      an API.
-                    </p>
-                  </div>
-                </div>
-              </div>
-
-              <div className="item">
-                <div className="team-data-img">
-                  <a
-                    href="./public/assets/img/project/origins.png"
-                    target="_blank"
-                  >
-                    <div className="single-work">
-                      <img
-                        src="./public/assets/img/project/origins.png"
-                        alt="team"
-                        className="img-responsive"
-                        data-no-retina
-                      />
-                      <div className="overlay-text center-block">
-                        <div className="cases-image-inner">
-                          <span className="cases-line top" />
-                          <span className="cases-line top-right" />
-                          <span className="cases-line bottom" />
-                          <span className="cases-line bottom-left" />
-                          <h6 className="text-uppercase alt-font text-light">
-                            Origins E-sport
-                          </h6>
-                        </div>
-                      </div>
-                    </div>
-                  </a>
-                  <div className="portfolio-content">
-                    <h5 className="text-blue main-font text-uppercase mb-2">
-                      Origins E-sport
-                    </h5>
-                    <p className="text-capitalize">Currently in development</p>
-                  </div>
+                </a>
+                <div className="portfolio-content">
+                  <h5 className="text-blue main-font text-uppercase mb-2">
+                    WildBook - My First Website
+                  </h5>
+                  <p>
+                    Responsive and mobile-first single-page site using HTML,
+                    CSS, and vanilla JS. <br />
+                    This project was made in 2 weeks with only 2 weeks of
+                    experience in web dev
+                  </p>
                 </div>
               </div>
             </div>
-            {/* <a className="circle ini-customPrevBtn" id="team-circle-left">
-              <img
-                src="./public/assets/icons/portfolio/arrow-left-circle.svg"
-                alt="arrow left"
-              />
-            </a>
-            <a className="circle ini-customNextBtn" id="team-circle-right">
-              <img
-                src="./public/assets/icons/portfolio/arrow-right-circle.svg"
-                alt="arrow right"
-              />
-            </a> */}
-          </div>
-        </div>
-      </div>
+
+            <div className="item">
+              <div className="team-data-img">
+                <a
+                  href="https://arkantik.github.io/hogwarts-magic-cards/"
+                  target="_blank"
+                  rel="noreferrer"
+                >
+                  <div className="single-work">
+                    <img
+                      src="./public/assets/img/project/hogwarts.png"
+                      alt="Hogwarts magic cards project"
+                      className="img-responsive"
+                      data-no-retina
+                    />
+                    <div className="overlay-text center-block">
+                      <div className="cases-image-inner">
+                        <span className="cases-line top" />
+                        <span className="cases-line top-right" />
+                        <span className="cases-line bottom" />
+                        <span className="cases-line bottom-left" />
+                        <h6 className="text-uppercase alt-font text-light">
+                          Hogwarts Magic Cards
+                        </h6>
+                      </div>
+                    </div>
+                  </div>
+                </a>
+                <div className="portfolio-content">
+                  <h5 className="text-blue main-font text-uppercase mb-2">
+                    Hogwarts Magic Cards
+                  </h5>
+                  <p>
+                    Flash-game type website using ReactJS, a CSS
+                    framework(Tailwind), and integrating an API.
+                    <br />
+                    This website is a strategy turn by turn card game set in the
+                    world of Harry Potter.
+                  </p>
+                </div>
+              </div>
+            </div>
+
+            <div className="item">
+              <div className="team-data-img">
+                <a
+                  href="https://arkantik.github.io/holifun/"
+                  target="_blank"
+                  rel="noreferrer"
+                >
+                  <div className="single-work">
+                    <img
+                      src="./public/assets/img/project/holifun.png"
+                      alt="Holifun project"
+                      className="img-responsive"
+                      data-no-retina
+                    />
+                    <div className="overlay-text center-block">
+                      <div className="cases-image-inner">
+                        <span className="cases-line top" />
+                        <span className="cases-line top-right" />
+                        <span className="cases-line bottom" />
+                        <span className="cases-line bottom-left" />
+                        <h6 className="text-uppercase alt-font text-light">
+                          HoliFun
+                        </h6>
+                      </div>
+                    </div>
+                  </div>
+                </a>
+                <div className="portfolio-content">
+                  <h5 className="text-blue main-font text-uppercase mb-2">
+                    HoliFun
+                  </h5>
+                  <p>
+                    No idea where to go for your next vacation? HoliFun is made
+                    for you!
+                    <br />
+                    This project has been created during a 36h Hackathon, with
+                    the objective to build something about vacation and using an
+                    API.
+                  </p>
+                </div>
+              </div>
+            </div>
+
+            <div className="item">
+              <div className="team-data-img">
+                <a
+                  href="./public/assets/img/project/origins.png"
+                  target="_blank"
+                >
+                  <div className="single-work">
+                    <img
+                      src="./public/assets/img/project/origins.png"
+                      alt="team"
+                      className="img-responsive"
+                      data-no-retina
+                    />
+                    <div className="overlay-text center-block">
+                      <div className="cases-image-inner">
+                        <span className="cases-line top" />
+                        <span className="cases-line top-right" />
+                        <span className="cases-line bottom" />
+                        <span className="cases-line bottom-left" />
+                        <h6 className="text-uppercase alt-font text-light">
+                          Origins E-sport
+                        </h6>
+                      </div>
+                    </div>
+                  </div>
+                </a>
+                <div className="portfolio-content">
+                  <h5 className="text-blue main-font text-uppercase mb-2">
+                    Origins E-sport
+                  </h5>
+                  <p className="text-capitalize">Currently in development</p>
+                </div>
+              </div>
+            </div>
+          </div> 
+        </div> */}
     </section>
   );
 }
