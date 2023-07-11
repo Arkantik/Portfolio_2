4da887d67cb7fcefe540f4bb0c44d14fd3bfcd40
Jeremy Dohin <arkantik.pro@gmail.com>
2023-07-11 12:12:21 +0200

    Removed timeline component

--- frontend/src/pages/Portfolio.jsx
+++ frontend/src/pages/Portfolio.jsx
@@ -1,13 +1,11 @@
 import Home from "../components/Home";
 import Project from "../components/Project";
 import Recommendation from "../components/Recommendation";
-// import Timeline from "../components/Timeline";
 
 export default function Portfolio() {
   return (
     <div>
       <Home />
-      {/* <Timeline /> */}
       <Project />
       <Recommendation />
     </div>
