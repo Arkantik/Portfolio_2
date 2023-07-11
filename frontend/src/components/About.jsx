70b9f5ff4e712d6f629d0aae9cd70ba7604e97de
Jeremy Dohin <arkantik.pro@gmail.com>
2023-07-11 16:25:33 +0200

    Creation of about page

---
+++ frontend/src/components/About.jsx
@@ -0,0 +1,21 @@
+export default function About() {
+  return (
+    <section
+      className="flex h-screen flex-col justify-center gap-16 font-alt"
+      id="about"
+    >
+      <div>
+        <h2 className="block font-main text-xl uppercase text-light">
+          <span className="block uppercase text-primary"> About me </span>
+          <span> &amp; my skills </span>
+        </h2>
+      </div>
+      <div>
+        <h2 className="block font-main text-xl uppercase text-light">
+          <span className="block uppercase text-primary"> About me </span>
+          <span> & my skills </span>
+        </h2>
+      </div>
+    </section>
+  );
+}
