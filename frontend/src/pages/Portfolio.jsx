3c2e994abb3b8eee68e61d66b3595bc15270b85e
Jeremy Dohin <arkantik.pro@gmail.com>
2023-07-11 16:26:08 +0200

    Added new about and contact pages to portfolio

--- frontend/src/pages/Portfolio.jsx
+++ frontend/src/pages/Portfolio.jsx
@@ -1,13 +1,17 @@
 import Home from "../components/Home";
+import About from "../components/About";
 import Project from "../components/Project";
 import Review from "../components/Review";
+import Contact from "../components/Contact";
 
 export default function Portfolio() {
   return (
     <div>
       <Home />
+      <About />
       <Project />
       <Review />
+      <Contact />
     </div>
   );
 }
