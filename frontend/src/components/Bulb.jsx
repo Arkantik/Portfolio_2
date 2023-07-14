be4a212f29c008adbf33569f31d783b1d70b0a1d
Jeremy Dohin <arkantik.pro@gmail.com>
2023-07-14 15:11:26 +0200

    Fixed z-index of bulb

--- frontend/src/components/Bulb.jsx
+++ frontend/src/components/Bulb.jsx
@@ -1,6 +1,6 @@
 export default function Bulb() {
   return (
-    <div className="relative -left-36 bottom-72 z-30 w-[200px] rotate-12 animate-pulse mix-blend-color-dodge duration-75 md:bottom-96 md:w-[260px]">
+    <div className="relative -left-36 bottom-72 z-10 w-[200px] rotate-12 animate-pulse mix-blend-color-dodge duration-75 md:bottom-96 md:w-[260px]">
       <img src="../assets/img/bulb.png" alt="bulb" />
     </div>
   );
