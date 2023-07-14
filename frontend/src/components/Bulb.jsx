bf03b78f40205f3d12e1f3689a968f4bf4efb752
Jeremy Dohin <arkantik.pro@gmail.com>
2023-07-14 14:57:01 +0200

    Added bulb on about page

---
+++ frontend/src/components/Bulb.jsx
@@ -0,0 +1,7 @@
+export default function Bulb() {
+  return (
+    <div className="relative -left-36 bottom-72 z-30 w-[200px] rotate-12 animate-pulse mix-blend-color-dodge duration-75 md:bottom-96 md:w-[260px]">
+      <img src="../assets/img/bulb.png" alt="bulb" />
+    </div>
+  );
+}
--- frontend/src/pages/About.jsx
+++ frontend/src/pages/About.jsx
@@ -1,21 +1,30 @@
+// Components
+import Bulb from "../components/Bulb";
+
+// Framer motion
+// import { motion } from "framer-motion";
+
 export default function About() {
   return (
-    <section
-      className="flex h-screen flex-col justify-center gap-16 font-alt"
-      id="about"
-    >
-      <div>
-        <h2 className="block font-main text-xl uppercase text-light">
-          <span className="block uppercase text-primary"> About me </span>
-          <span> &amp; my skills </span>
-        </h2>
-      </div>
-      <div>
-        <h2 className="block font-main text-xl uppercase text-light">
-          <span className="block uppercase text-primary"> About me </span>
-          <span> & my skills </span>
-        </h2>
-      </div>
-    </section>
+    <>
+      <section
+        className="flex h-screen flex-col justify-center gap-16 font-alt"
+        id="about"
+      >
+        <div>
+          <h2 className="block font-main text-xl uppercase text-light">
+            <span className="block uppercase text-primary"> About me </span>
+            <span> &amp; my skills </span>
+          </h2>
+        </div>
+        <div>
+          <h2 className="block font-main text-xl uppercase text-light">
+            <span className="block uppercase text-primary"> About me </span>
+            <span> & my skills </span>
+          </h2>
+        </div>
+      </section>
+      <Bulb />
+    </>
   );
 }
