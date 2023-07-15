8b088d193450ecec4005f383b4b8fde140f1fb2e
Jeremy Dohin <arkantik.pro@gmail.com>
2023-07-16 01:49:04 +0200

    Updated about section with toggle component and skills/tools

--- frontend/src/pages/About.jsx
+++ frontend/src/pages/About.jsx
@@ -1,10 +1,23 @@
+// Packages
+import { useState } from "react";
+
 // Components
 import Bulb from "../components/Bulb";
+import Toggle from "../components/utilities/Toggle";
 
 // Framer motion
 // import { motion } from "framer-motion";
 
+// Data
+import { skills, tools } from "../helpers/AboutData";
+
 export default function About() {
+  const [activeTab, setActiveTab] = useState("skills");
+
+  const handleTabToggle = (tab) => {
+    setActiveTab(tab);
+  };
+
   return (
     <>
       <section
@@ -17,12 +30,45 @@ export default function About() {
             <span> &amp; my skills </span>
           </h2>
         </div>
-        <div>
-          <h2 className="block font-main text-xl uppercase text-light">
-            <span className="block uppercase text-primary"> About me </span>
-            <span> & my skills </span>
-          </h2>
-        </div>
+
+        <article className="grid grid-cols-1 items-center gap-8 lg:grid-cols-2">
+          <div>
+            <p className="text-base text-light">
+              I develop simple, intuitive and responsive user interface that
+              helps users get things done with less effort and time with those
+              technologies.
+            </p>
+          </div>
+          <div className="flex flex-col gap-8">
+            <Toggle activeTab={activeTab} onTabToggle={handleTabToggle} />
+
+            <div className="flex w-full flex-wrap justify-center">
+              {activeTab === "skills" && (
+                <ul>
+                  {skills.map((skill) => (
+                    <li key={skill.id} className="m-1 inline-flex">
+                      <div className="rounded-lg bg-light p-2">
+                        <div className="text-2xl">{skill.icon}</div>
+                      </div>
+                    </li>
+                  ))}
+                </ul>
+              )}
+
+              {activeTab === "tools" && (
+                <ul>
+                  {tools.map((tool) => (
+                    <li key={tool.id} className="m-1 inline-flex">
+                      <div className="rounded-lg bg-light p-2">
+                        <div className="text-2xl">{tool.icon}</div>
+                      </div>
+                    </li>
+                  ))}
+                </ul>
+              )}
+            </div>
+          </div>
+        </article>
       </section>
       <Bulb />
     </>
