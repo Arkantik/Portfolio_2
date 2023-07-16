e4eee2344c0a2ac6a39a2343872f6572706b3c7e
Jeremy Dohin <arkantik.pro@gmail.com>
2023-07-16 22:01:29 +0200

    Moved assets in different folders, languages and tools

---
+++ frontend/src/helpers/aboutData.js
@@ -0,0 +1,25 @@
+const skills = [
+  { id: 1, name: "html", icon: "../assets/icons/languages/html.svg" },
+  { id: 2, name: "css", icon: "../assets/icons/languages/css.svg" },
+  { id: 3, name: "css", icon: "../assets/icons/languages/js.svg" },
+  { id: 4, name: "react", icon: "../assets/icons/languages/react.svg" },
+  { id: 5, name: "vitejs", icon: "../assets/icons/languages/vitejs.svg" },
+  { id: 6, name: "tailwind", icon: "../assets/icons/languages/tailwind.svg" },
+  { id: 7, name: "sass", icon: "../assets/icons/languages/sass.svg" },
+  { id: 8, name: "nodejs", icon: "../assets/icons/languages/nodejs.svg" },
+  { id: 9, name: "express", icon: "../assets/icons/languages/express.svg" },
+  { id: 10, name: "mysql", icon: "../assets/icons/languages/mysql.svg" },
+];
+
+const tools = [
+  { id: 1, name: "git", icon: "../assets/icons/tools/git.svg" },
+  { id: 2, name: "vscode", icon: "../assets/icons/tools/vscode.svg" },
+  { id: 3, name: "npm", icon: "../assets/icons/tools/npm.svg" },
+  { id: 4, name: "figma", icon: "../assets/icons/tools/figma.svg" },
+  { id: 5, name: "clickup", icon: "../assets/icons/tools/clickup.svg" },
+  { id: 6, name: "slack", icon: "../assets/icons/tools/slack.svg" },
+  { id: 7, name: "insomnia", icon: "../assets/icons/tools/insomnia.svg" },
+  { id: 8, name: "discord", icon: "../assets/icons/tools/discord.svg" },
+];
+
+export { skills, tools };
