9fbd90d3bf88989e7030d475fb7d5a67ce18f226
Jeremy Dohin <arkantik.pro@gmail.com>
2023-07-19 00:33:13 +0200

    Removed bulb from about section

--- frontend/src/pages/About.jsx
+++ frontend/src/pages/About.jsx
@@ -2,7 +2,6 @@
 import { useState } from "react";
 
 // Components
-import Bulb from "../components/Bulb";
 import Toggle from "../components/utilities/Toggle";
 import Accordion from "../components/Accordion";
 
@@ -17,61 +16,63 @@ export default function About() {
   };
 
   return (
-    <>
-      <section
-        className="z-10 flex min-h-[calc(100dvh-80px)] flex-col justify-center font-alt"
-        id="about"
-      >
-        <h2 className="block font-main text-xl uppercase text-light">
-          <span className="block uppercase text-primary"> About me </span>
-          <span> &amp; my skills </span>
-        </h2>
+    <section
+      className="flex min-h-[calc(100dvh-80px)] flex-col justify-center font-alt text-light"
+      id="about"
+    >
+      <h2 className="block font-main text-xl uppercase">
+        <span className="block uppercase text-primary"> About me </span>
+        <span> &amp; my skills </span>
+      </h2>
 
-        <article className="grid grid-cols-1 items-center gap-4 md:grid-cols-2">
-          <p className="text-justify text-sm text-light">
-            Using all these technologies and tools, I am able to develop simple,
-            intuitive and responsive user interface that helps users get things
-            done with less effort and time.
-          </p>
+      <article className="z-10 grid grid-cols-1 items-center gap-4 md:grid-cols-2">
+        <p className="text-justify text-sm ">
+          Using all these technologies and tools, I am able to develop simple,
+          intuitive and responsive user interface that helps users get things
+          done with less effort and time.
+        </p>
 
-          {/* Skills and tools section */}
-          <div className="flex flex-col items-end gap-8">
-            <Toggle activeTab={activeTab} onTabToggle={handleTabToggle} />
-            {activeTab === "skills" && (
-              <ul className="flex w-full flex-wrap justify-center">
-                {skills.map((skill) => (
-                  <li key={skill.id} className="m-2 inline-flex">
-                    <div className="rounded-lg border-l border-t border-light/30 bg-light/10 p-2 backdrop-blur-sm">
-                      <img src={skill.icon} alt={skill.name} className="w-14" />
-                    </div>
-                  </li>
-                ))}
-              </ul>
-            )}
-            {activeTab === "tools" && (
-              <ul className="flex w-full flex-wrap justify-center">
-                {tools.map((tool) => (
-                  <li key={tool.id} className="m-2 inline-flex">
-                    <div className="rounded-lg border-l border-t border-light/30 bg-light/10 p-2 backdrop-blur-sm">
-                      <img src={tool.icon} alt={tool.name} className="w-14" />
-                    </div>
-                  </li>
-                ))}
-              </ul>
-            )}
-          </div>
-        </article>
-        <article className="grid grid-cols-1 items-center gap-8 md:grid-cols-2">
-          {/* Hobbies section */}
+        {/* Skills and tools section */}
+        <div className="flex flex-col items-end gap-8">
+          <Toggle activeTab={activeTab} onTabToggle={handleTabToggle} />
+          {activeTab === "skills" && (
+            <ul className="flex w-full flex-wrap justify-center">
+              {skills.map((skill) => (
+                <li key={skill.id} className="m-2 inline-flex">
+                  <div className="rounded-lg border-l border-t border-light/30 bg-light/10 p-2 backdrop-blur-sm">
+                    <img src={skill.icon} alt={skill.name} className="w-14" />
+                  </div>
+                </li>
+              ))}
+            </ul>
+          )}
+          {activeTab === "tools" && (
+            <ul className="flex w-full flex-wrap justify-center">
+              {tools.map((tool) => (
+                <li key={tool.id} className="m-2 inline-flex">
+                  <div className="rounded-lg border-l border-t border-light/30 bg-light/10 p-2 backdrop-blur-sm">
+                    <img src={tool.icon} alt={tool.name} className="w-14" />
+                  </div>
+                </li>
+              ))}
+            </ul>
+          )}
+        </div>
+      </article>
+      <hr />
+      <article className="grid grid-cols-1 items-center gap-y-4 md:grid-flow-col-dense md:grid-cols-3">
+        {/* Hobbies section */}
+        <p className="text-justify text-sm md:order-2 md:col-span-1">
+          My hobbies and interests encompass a diverse range of fields. I find
+          delight in diving the fascinating world of cryptocurrency, embarking
+          on exciting journeys around the world, immersing myself in the virtual
+          realms of gaming, and unleashing my creativity through design
+          projects.
+        </p>
+        <div className="md:order-1 md:col-span-2">
           <Accordion />
-          <p className="text-justify text-sm text-light">
-            Using all these technologies and tools, I am able to develop simple,
-            intuitive and responsive user interface that helps users get things
-            done with less effort and time.
-          </p>
-        </article>
-      </section>
-      <Bulb />
-    </>
+        </div>
+      </article>
+    </section>
   );
 }
