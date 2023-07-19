001bdb00fb2423ea33f76971d648f025fa062e14
Jeremy Dohin <arkantik.pro@gmail.com>
2023-07-19 22:21:25 +0200

    Added auth logic into main and app.jsx = deleted previous data.json

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,20 +1,46 @@
 // Packages
+import { Routes, Route } from "react-router-dom";
+import { useEffect } from "react";
 
 // Components
 import Navbar from "./components/utilities/Navbar";
 
+// Hooks
+import useAuth from "./hooks/useAuth";
+
 // Pages
 import Portfolio from "./pages/Portfolio";
+import Login from "./pages/Login";
+import NotFound from "./pages/NotFound";
 
 // Style
 import "./App.css";
+import Dashboard from "./pages/Dashboard";
 
 export default function App() {
+  const { isLoggedIn, loadUserFromLocalStorage } = useAuth();
+
+  useEffect(() => {
+    loadUserFromLocalStorage();
+  }, []);
+
   return (
     <div className="overflow-hidden bg-dark">
       <Navbar />
       <main>
-        <Portfolio />
+        <Routes>
+          <Route path="/" element={<Portfolio />} />
+          {isLoggedIn ? (
+            <Route>
+              <Route path="dashboard" element={<Dashboard />} />
+            </Route>
+          ) : (
+            <Route>
+              <Route path="login" element={<Login />} />
+            </Route>
+          )}
+          <Route path="*" element={<NotFound />} />
+        </Routes>
       </main>
     </div>
   );
