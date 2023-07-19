e66236d5257e14bc22e90391771272ca4c333c4b
Jeremy Dohin <arkantik.pro@gmail.com>
2023-07-19 14:20:32 +0200

    Creation of project manager

---
+++ backend/src/models/ProjectManager.js
@@ -0,0 +1,43 @@
+const AbstractManager = require("./AbstractManager");
+
+class ProjectManager extends AbstractManager {
+  constructor() {
+    super({ table: "project" });
+  }
+
+  create(body) {
+    const {
+      name,
+      description,
+      img,
+      github,
+      website,
+      tool,
+      techno_id: technoID,
+    } = body;
+
+    return this.database.query(
+      `INSERT INTO ${this.table} (name, description, img, github, website, tool, techno_id) VALUES (?, ?, ?, ?, ?, ?, ?)`,
+      [name, description, img, github, website, tool, technoID]
+    );
+  }
+
+  update(body, id) {
+    const {
+      name,
+      description,
+      img,
+      github,
+      website,
+      tool,
+      techno_id: technoID,
+    } = body;
+
+    return this.database.query(
+      `UPDATE ${this.table} SET name = ?, upload_date = ?, description = ?, img = ?, github = ?, website = ?, tool, techno_id: technoID = ?, WHERE id = ?`,
+      [name, description, img, github, website, tool, technoID, id]
+    );
+  }
+}
+
+module.exports = ProjectManager;
