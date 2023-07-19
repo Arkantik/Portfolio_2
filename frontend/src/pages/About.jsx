bf3f07d24d45a6cf3a04ad0164aaec9bb981945f
Jeremy Dohin <arkantik.pro@gmail.com>
2023-07-19 10:09:46 +0200

    Added intersection observer on about section

--- frontend/src/pages/About.jsx
+++ frontend/src/pages/About.jsx
@@ -1,5 +1,5 @@
 // Packages
-import { useState } from "react";
+import { useState, useEffect, useRef } from "react";
 
 // Components
 import Toggle from "../components/utilities/Toggle";
@@ -11,32 +11,77 @@ import { skills, tools } from "../helpers/aboutData";
 export default function About() {
   const [activeTab, setActiveTab] = useState("skills");
 
+  const aboutH2 = useRef(null);
+  const skillIcons = useRef(null);
+  const skillP = useRef(null);
+
+  const hobbyAccordion = useRef(null);
+  const hobbyP = useRef(null);
+
   const handleTabToggle = (tab) => {
     setActiveTab(tab);
   };
+
+  useEffect(() => {
+    const observers = [
+      { ref: aboutH2, classToAdd: "show-el" },
+      { ref: skillIcons, classToAdd: "show-item" },
+      { ref: skillP, classToAdd: "show-item" },
+
+      { ref: hobbyAccordion, classToAdd: "show-item" },
+      { ref: hobbyP, classToAdd: "show-item" },
+    ].map(({ ref, classToAdd }) => {
+      const observer = new IntersectionObserver((entries) => {
+        entries.forEach((entry) => {
+          if (entry.isIntersecting) {
+            entry.target.classList.add(classToAdd);
+          } else {
+            entry.target.classList.remove(classToAdd);
+          }
+        });
+      });
+      observer.observe(ref.current);
+
+      return {
+        observer,
+        element: ref.current,
+      };
+    });
+
+    return () => {
+      observers.forEach(({ observer, element }) => {
+        observer.unobserve(element);
+      });
+    };
+  }, []);
 
   return (
     <section
       className="flex min-h-[calc(100dvh-80px)] flex-col justify-center font-alt text-light"
       id="about"
     >
-      <h2 className="block font-main text-xl uppercase">
+      <h2
+        className="hidden-title block font-main text-xl uppercase"
+        ref={aboutH2}
+      >
         <span className="block uppercase text-primary"> About me </span>
         <span> &amp; my skills </span>
       </h2>
 
+      {/* Skills and tools section */}
       <article className="z-10 grid grid-cols-1 items-center gap-4 md:grid-cols-2">
-        <p className="text-justify text-sm ">
+        <p className="hidden-left text-justify text-sm " ref={skillP}>
           Using all these technologies and tools, I am able to develop simple,
           intuitive and responsive user interface that helps users get things
           done with less effort and time.
         </p>
-
-        {/* Skills and tools section */}
-        <div className="flex flex-col items-end gap-8">
+        <div
+          className="hidden-right flex flex-col items-end gap-8"
+          ref={skillIcons}
+        >
           <Toggle activeTab={activeTab} onTabToggle={handleTabToggle} />
           {activeTab === "skills" && (
-            <ul className="flex w-full flex-wrap justify-center">
+            <ul className="  flex w-full flex-wrap justify-center">
               {skills.map((skill) => (
                 <li key={skill.id} className="m-2 inline-flex">
                   <div className="rounded-lg border-l border-t border-light/30 bg-light/10 p-2 backdrop-blur-sm">
@@ -59,17 +104,25 @@ export default function About() {
           )}
         </div>
       </article>
+
       <hr />
-      <article className="grid grid-cols-1 items-center gap-y-4 md:grid-flow-col-dense md:grid-cols-3">
-        {/* Hobbies section */}
-        <p className="text-justify text-sm md:order-2 md:col-span-1">
+
+      {/* Hobbies section */}
+      <article className="grid grid-cols-1 items-center gap-y-4 md:grid-cols-3 ">
+        <p
+          className="hidden-right text-justify text-sm md:order-2 md:col-span-1"
+          ref={hobbyP}
+        >
           My hobbies and interests encompass a diverse range of fields. I find
           delight in diving the fascinating world of cryptocurrency, embarking
           on exciting journeys around the world, immersing myself in the virtual
           realms of gaming, and unleashing my creativity through design
           projects.
         </p>
-        <div className="md:order-1 md:col-span-2">
+        <div
+          className="hidden-left md:order-1 md:col-span-2"
+          ref={hobbyAccordion}
+        >
           <Accordion />
         </div>
       </article>
