df8f0efab4ed43763fd2ee5a5027b3215ae6cfa8
Jeremy Dohin <arkantik.pro@gmail.com>
2023-07-20 14:15:49 +0200

    Update of dashboard with main component

--- frontend/src/pages/Dashboard.jsx
+++ frontend/src/pages/Dashboard.jsx
@@ -1,9 +1,12 @@
+// Packages
+import { useState } from "react";
+
+// Components
+import ManageProject from "../components/dashboard/ManageProject";
+
 export default function Dashboard() {
+  const [filterText, setFilterText] = useState("");
   return (
-    <section className="flex h-screen min-h-[calc(100dvh-80px)] flex-col items-center justify-center gap-8 font-alt text-light md:gap-16 lg:flex-row">
-      <div className="flex flex-col gap-4 text-center">
-        <p className="text-xl md:text-2xl">DASHBOARD</p>
-      </div>
-    </section>
+    <ManageProject filterText={filterText} setFilterText={setFilterText} />
   );
 }
